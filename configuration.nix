4b6b0ba94d255dda24cd3dfb77626a7a616a6501
JU12000 <jowilliams12000@gmail.com>
2024-11-19 13:17:47 -0500

    init

---
+++ configuration.nix
@@ -0,0 +1,119 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hosts/Tango/hardware-configuration.nix
+    ];
+
+  # Enable Nix Flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.useOSProber = true;
+
+  # Setup keyfile
+  boot.initrd.secrets = {
+    "/boot/crypto_keyfile.bin" = null;
+  };
+
+  boot.loader.grub.enableCryptodisk = true;
+
+  boot.initrd.luks.devices."luks-d8d76b31-fdde-4696-93bd-68b2a6770908".keyFile = "/boot/crypto_keyfile.bin";
+
+  networking.hostName = "Tango"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Disallow imperative user management.
+  users.mutableUsers = false;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.ju12000 = {
+    isNormalUser = true;
+    description = "ju12000";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+    hashedPassword = "$y$j9T$KRSYlFHbuJEIuf.q5onWL1$8Aa48qXnQz1PIbZHVY7OQ4oQ5fFI8v7.Z5br2MC263C";
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+    kitty
+    vscodium
+    vscode-extensions.mkhl.direnv
+    git
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+  # Automatically run nix-collect-garbage every day at midnight.
+  nix.gc.automatic = true;
+  nix.gc.dates = "00:00";
+
+  # Hyprland
+  programs.hyprland.enable = true;
+  # Hint Electron apps to use Wayland
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+}
---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "Main Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    disko.url = "github:nix-community/disko/latest"
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, ... }@attrs: {
+    nixosConfigurations.Tango = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = attrs;
+      modules = [ 
+        ./configuration.nix
+        disko.nixosModules.disko {
+          disko.devices = {
+            disk = {
+              main = {
+                device = <disk-device>;
+                type = "disk";
+                content = {
+                  type = "gpt";
+                  partitions = {
+                    ESP = {
+                      size = "500M";
+                      type = "EF00";
+                      content = {
+                        type = "filesystem";
+                        format = "vfat";
+                        mountpoint = "/boot";
+                        mountOptions = [ "umask=0077" ];
+                      };
+                    };
+                    luks = {
+                      size = "100%";
+                      content = {
+                        type = "luks";
+                        name = "crypted";
+                        settings.allowDiscards = true;
+                        passwordFile = "/tmp/secret.key";
+                        content = {
+                          type = "filesystem";
+                          format = "ext4";
+                          mountpoint = "/";
+                        };
+                      };
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      ];
+    };
+  };
+}
