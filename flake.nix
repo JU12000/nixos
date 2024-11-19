e1e0bc90dee7076b0f004f32897384664321ec11
JU12000 <jowilliams12000@gmail.com>
2024-11-19 18:41:29 +0000

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-    disko.url = "github:nix-community/disko/latest"
+    disko.url = "github:nix-community/disko/latest";
     disko.inputs.nixpkgs.follows = "nixpkgs";
   };
 
