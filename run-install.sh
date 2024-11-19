df8f72059c86d2a1ef7ecc40640564a7d2942d42
JU12000 <jowilliams12000@gmail.com>
2024-11-19 18:40:38 +0000

    update run-install.

--- run-install.sh
+++ run-install.sh
@@ -1,1 +1,1 @@
-sudo nix --extra-experimental-features nix-command run 'github:nix-community/disko/latest#disko-install' -- --flake $1 --disk $2 $3
+sudo nix --extra-experimental-features "nix-command flakes" run 'github:nix-community/disko/latest#disko-install' -- --flake $1 --disk $2 $3
